bca7b2b6e0f059dffdb47c8004a2ee9c36456e18
tomkallen <spbeat@mail.ru>
2016-11-24 10:06:01 +0300

    removed unnecessary variables from code

--- js/script.js
+++ js/script.js
@@ -1,48 +1,36 @@
 var cur = 0,
-	container = document.getElementsByClassName('slider-inner'),
-	slide = document.getElementsByClassName('slide'),
-	controls = document.getElementsByClassName('controls'),
-	prevBtn = document.getElementsByClassName('controls-btn_prev'),
-	nextBtn = document.getElementsByClassName('controls-btn_next'),
-	slideWidth = slide[cur].offsetWidth;
-	numOfSlides = slide.length;
+    container = document.getElementsByClassName('slider-inner'),
+    slide = document.getElementsByClassName('slide'),
+    controls = document.getElementsByClassName('controls'),
+    prevBtn = document.getElementsByClassName('controls-btn_prev'),
+    nextBtn = document.getElementsByClassName('controls-btn_next'),
+    slideWidth = slide[cur].offsetWidth;
+numOfSlides = slide.length;
 
 for (var i = cur; i < numOfSlides; i++) {
-	slide[i].style.setProperty('left', slideWidth * i + 'px');
+    slide[i].style.setProperty('left', slideWidth * i + 'px');
 }
 
 function changeSlide(direction) {
-	switch(direction) {
-		case 'left':
-			cur--;
-			if(cur < 0) cur = numOfSlides - 1;
-			console.log('left', cur);
-			break;
+    switch (direction) {
+        case 'left':
+            cur--;
+            if (cur < 0) cur = numOfSlides - 1;
+            console.log('left', cur);
+            break;
 
-		case 'right':
-			cur++;
-			if(cur == numOfSlides) cur = 0;
-			console.log('right', cur);
-			break;
-	}
-	container[0].style.setProperty('transform', 'translate3d(' + (-slideWidth * cur) + 'px, 0, 0');
+        case 'right':
+            cur++;
+            if (cur == numOfSlides) cur = 0;
+            console.log('right', cur);
+            break;
+    }
+    container[0].style.setProperty('transform', 'translate3d(' + (-slideWidth * cur) + 'px, 0, 0');
 };
 
 controls[0].addEventListener('click', function(e) {
-	var target = e.target,
-		direction = 'right';
 
-	if( target == prevBtn[0] ) {
-		
-		direction = 'left';
-		changeSlide(direction);
-	}
+    e.target == prevBtn[0] && changeSlide('left');
+    e.target == nextBtn[0] && changeSlide('right');
 
-	if( target == nextBtn[0] ) {
-		
-		direction = 'right';
-		changeSlide(direction);
-	}
 });
-
-
